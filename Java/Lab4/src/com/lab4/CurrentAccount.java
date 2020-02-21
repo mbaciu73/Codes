6ceb93fc70d4ed2da67bf1393562e8cb9503c07e
mbaciu73 <55544146+mbaciu73@users.noreply.github.com>
2020-02-21 17:12:15 +0000

    Update CurrentAccount.java

--- Java/Lab4/src/com/lab4/CurrentAccount.java
+++ Java/Lab4/src/com/lab4/CurrentAccount.java
@@ -1,5 +1,5 @@
 package com.lab4;
-
+//this is for a current account which allows money to be taken out if sufficient funds is available
 public class CurrentAccount extends Account {
 	private double penaltyAmount;
 
@@ -11,8 +11,7 @@ public class CurrentAccount extends Account {
 		super(accountName, sortCode, branchName, inCredit, acctBalance);
 		
 	}
-	
-	
+	//there must be enough money inside the account in order for a withdraw to be done
 	public void withdraw(double takeAmount) {
 		if(getAcctBalance()-takeAmount<0) {
 			System.out.println("Insufficient Funds");
@@ -21,13 +20,14 @@ public class CurrentAccount extends Account {
 			setAcctBalance(getAcctBalance()-takeAmount);
 		}
 	}
+	//this checks if sufficient funds is in the account
 	public String checkCredit() {
 		if(getAcctBalance()<0) {
 			return "Your account balance is below zero";
 		}
 		return "Your account balance is above zero";
 	}
-	
+	//if a warning message is entered it will return the message if there is less than 100 in the account
 	public String checkCredit(String warningmessage) {
 		if(getAcctBalance()<100 && getAcctBalance()>0) 
 		{
@@ -35,6 +35,7 @@ public class CurrentAccount extends Account {
 		}
 		return "Your balance is fine";
 	}
+	//returns the super string and the penalty amount too
 	public String toString() {
 		return super.toString()+" and the penalty amount is "+penaltyAmount;
 	}
